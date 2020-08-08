feb5e433185b7dff1b9fcd8ba3b5384251b13107
HaykEminyan <ibhayk@gmail.com>
2020-08-08 14:27:01 +0300

    final

--- BigInteger Lib/Main.java
+++ BigInteger Lib/Main.java
@@ -14,14 +14,16 @@ class Main {
 		}
 		return factor;
 	}
+
 	public static BigInteger power(int exp) {
 		BigInteger b1 = new BigInteger("12321000");
 		BigInteger res = b1.pow(exp);
 		return res;
-    }
-    public static BigInteger multiply() {
-        BigInteger one = new BigInteger("231243143214233123415");
-        BigInteger two = new BigInteger("3434315425352154314232");
-        return one.multiply(two);
-    }
+	}
+
+	public static BigInteger multiply() {
+		BigInteger one = new BigInteger("231243143214233123415");
+		BigInteger two = new BigInteger("3434315425352154314232");
+		return one.multiply(two);
+	}
 }
